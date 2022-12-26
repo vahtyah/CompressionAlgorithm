f4da30a77e00578ac062e116501a26371e653227
Май Ван Туан <86241573+8268826@users.noreply.github.com>
2022-12-26 19:47:52 +0300

    fix lz77

--- prac_8/LZ77.h
+++ prac_8/LZ77.h
@@ -5,78 +5,87 @@
 // LZ77 encoding function
 std::string encodeLZ77(const std::string& s)
 {
-    std::string result;
-    // Keep track of the current position in the input string
-    size_t i = 0;
-    while (i < s.size())
-    {
-        // Search backwards in the input string for the longest matching substring
-        size_t len = 0;
-        size_t offset = 0;
-        for (size_t j = i; j > 0; --j)
-        {
-            size_t k = 0;
-            while (s[i + k] == s[j + k] && k < i - j)
-            {
-                ++k;
-            }
-            if (k > len)
-            {
-                len = k;
-                offset = i - j;
-            }
-        }
+	std::string result;
+	// Keep track of the current position in the input string
+	size_t i = 0;
+	while (i < s.size())
+	{
+		// Search backwards in the input string for the longest matching substring
+		size_t len = 0;
+		size_t offset = 0;
+		for (size_t j = i; j > 0; --j)
+		{
+			size_t k = 0;
+			while (s[i + k] == s[j + k] && k < i - j)
+			{
+				++k;
+			}
+			if (k > len)
+			{
+				len = k;
+				offset = i - j;
+			}
+		}
+		if (len == 0)
+		{
+			result += s[i];
+			++i;
+		}
+		else {
 
-        // Append the matching substring to the result
-        result += '(' + std::to_string(offset) + ',' + std::to_string(len) + ')';
+			// Append the matching substring to the result
+			result += '(' + std::to_string(offset) + ',' + std::to_string(len) + ')';
 
-        // Append any remaining characters to the result
-        i += len;
-        while (i < s.size() && s[i] != s[i - len])
-        {
-            result += s[i];
-            ++i;
-        }
-    }
+			// Append any remaining characters to the result
+			i += len;
+			while (i < s.size() && s[i] != s[i - len])
+			{
+				result += s[i];
+				++i;
+			}
+		}
+	}
 
-    return result;
+	return result;
 }
+
+
 
 // LZ77 decoding function
 std::string decodeLZ77(const std::string& s)
 {
-    std::string result;
+	std::string result;
 
-    // Keep track of the current position in the input string
-    size_t i = 0;
-    while (i < s.size())
-    {
-        // Check if the next character is the start of a matching substring
-        if (s[i] == '(')
-        {
-            // Extract the offset and length of the matching substring
-            size_t j = s.find(',', i);
-            size_t offset = std::stoul(s.substr(i + 1, j - i - 1));
-            size_t k = s.find(')', j);
-            size_t len = std::stoul(s.substr(j + 1, k - j - 1));
+	// Keep track of the current position in the input string
+	size_t i = 0;
+	while (i < s.size())
+	{
+		// Check if the next character is the start of a matching substring
+		if (s[i] == '(')
+		{
+			// Extract the offset and length of the matching substring
+			size_t j = s.find(',', i);
+			size_t offset = std::stoul(s.substr(i + 1, j - i - 1));
+			size_t k = s.find(')', j);
+			size_t len = std::stoul(s.substr(j + 1, k - j - 1));
 
-            // Append the matching substring to the result
-            size_t m = result.size() - offset;
-            for (size_t n = 0; n < len; ++n)
-            {
-                result += result[m + n];
-            }
+			// Append the matching substring to the result
+			size_t m = result.size() - offset;
+			for (size_t n = 0; n < len; ++n)
+			{
+				result += result[m + n];
+			}
 
-            // Update the current position
-            i = k + 1;
-        }
-        else
-        {
-            // Append the next character to the result
-            result += s[i];
-            ++i;
-        }
-    }
+			// Update the current position
+			i = k + 1;
+		}
+		else
+		{
+			// Append the next character to the result
+			result += s[i];
+			++i;
+		}
+	}
 
-    return result;
+	return result;
 }
